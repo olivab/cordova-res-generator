370d5e9b2a2b67c0049e4d8eb8e807c443f80109
olivier.aboilard <olivier.aboilard@gmail.com>
2017-11-30 16:05:21 +0100

    Output layout tweak

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ var Gauge = require("gauge");
 
 var display = {
     info: (str) => {
-        console.log('  ' + str);
+        console.log(str);
     },
     success: (str) => {
-        str = 'V'.green + '  ' + str;
-        console.log('  ' + str);
+        str = ' ' + 'V'.green + ' ' + str;
+        console.log(str);
     },
     error: (str) => {
-        str = 'X'.red + '  ' + str;
-        console.log('  ' + str);
+        str = ' ' + 'X'.red + ' ' + str;
+        console.log(str);
     },
     header: (str) => {
         console.log('');
