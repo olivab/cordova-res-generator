0e24fde169b8b96a21a4f425ed5acd7c82d44031
olivier.aboilard <olivier.aboilard@gmail.com>
2017-11-30 16:52:11 +0100

    Layout changes

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var display = {
     },
     header: (str) => {
         console.log('');
-        console.log(str.underline);
+        console.log(str);
     }
 };
 
@@ -328,8 +328,9 @@ var g_settings = {
 
 // app entry point
 
-console.log("");
+console.log("***************************");
 console.log("cordova-res-generator " + pjson.version);
+console.log("***************************");
 
 check(g_settings)
     .then(() => generate(g_imageObjects, g_settings))
